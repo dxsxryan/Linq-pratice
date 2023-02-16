16ccda7d584da142b521ee1ee71ae0d48c52dd07
dxsxryan <dxsxryan@gmail.com>
2023-02-16 11:22:22 +0800

    加入專案檔案。

---
+++ Linq練習_1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq練習_1
+{
+    internal class Product
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+    }
+}
